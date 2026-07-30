c98a7b0a99d43dbf4d35fc40af90ed3a86b3eb30
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2026-07-30 20:15:31 +0900

    fix: use __dirname

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
 
 const packageJson = JSON.parse(
-  readFileSync("./package.json", "utf-8"),
+  readFileSync(resolve(__dirname, "package.json"), "utf-8"),
 );
 
 export default defineConfig({
