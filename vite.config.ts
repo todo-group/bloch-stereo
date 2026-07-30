4ea5a5bb9be324cb85d57a7a0c59edcb50bada20
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2026-07-30 19:47:02 +0900

    feat: add version info

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { readFileSync } from "node:fs";
+
+const packageJson = JSON.parse(
+  readFileSync("./package.json", "utf-8"),
+);
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(packageJson.version),
+  },
   base: process.env.BASE_PATH || "/",
 });
